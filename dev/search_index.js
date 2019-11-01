0b40e24ab3fa9e671aeebd1384dee6f1ca81f942
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-11-01 22:30:59 +0000

    build based on aa4e584

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MatrixPolynomials.jl-1","page":"Home","title":"MatrixPolynomials.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [MatrixPolynomials]","category":"page"},{"location":"#MatrixPolynomials.NewtonPolynomial","page":"Home","title":"MatrixPolynomials.NewtonPolynomial","text":"NewtonPolynomial(ζ, d)\n\nThe unique interpolation polynomial of a function in its Newton form, i.e.\n\nf(z) approx p(z) = sum_j=1^m fzeta_1zeta_j prod_i=1^j-1(z - zeta_i)\n\nwhere ζ are the interpolation points and d[j]=f[ζ₁,...,ζⱼ] is the jth divided difference of the interpolated function f.\n\n\n\n\n\n","category":"type"}]
+}
