8b37f386d78b02c475394fbac22f7b87b1702116
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2021-04-12 10:56:50 +0200

    Fixed documentation build

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,7 @@ makedocs(;
     repo="https://github.com/jagot/MatrixPolynomials.jl/blob/{commit}{path}#L{line}",
     sitename="MatrixPolynomials.jl",
     authors="Stefanos Carlström <stefanos.carlstrom@gmail.com>",
+    doctest=false,
 )
 
 deploydocs(;
