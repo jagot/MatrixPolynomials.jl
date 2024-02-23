97df6c3f078ddab136f1afc7da4abc2f2d7f1015
Documenter.jl <documenter@juliadocs.github.io>
2024-02-23 09:08:51 +0000

    build based on 11975c4

---
+++ v0.1.3/assets/latex.js
@@ -0,0 +1,32 @@
+requirejs.config({
+    paths: {
+        'mathjax': 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML',
+    },
+    shim: {
+        'mathjax' : {
+            exports: "MathJax"
+        },
+    }
+});
+
+require(['mathjax'], function(MathJax) {
+    MathJax.Hub.Config({
+        TeX: {
+            Macros: {
+                defd: "≝",
+                abs: ["\\left|#1\\right|",1],
+                vec: ["\\mathbf{#1}",1],
+                mat: ["\\mathsf{#1}",1],
+                conj: ["#1^*",1],
+                ce: "\\mathrm{e}",
+                im: "\\mathrm{i}",
+                diff : ["\\mathrm{d}#1\\,",1],
+                Beta : "\\mathrm{B}", // You've gotta be kidding
+                divdiff : "⏃",
+                bmat: ["\\begin{bmatrix}#1\\end{bmatrix}",1],
+                Real: "\\mathbb{R}",
+                Complex: "\\mathbb{C}"
+            }
+        }
+    });
+})
